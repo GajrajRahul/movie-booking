ef408e374a5fd6a2ec5ba47bde53e31ab7cc8076
Rahul Gajraj <rahulgajraj3497@gmail.com>
2023-03-10 10:17:48 +0530

    completed

---
+++ src/components/importent/api.js
@@ -0,0 +1,8 @@
+export const apiUrl = {
+  key: "ca25ef1c96f2c2f564eaf7149c9e333c",
+  base: "https://api.themoviedb.org/3/",
+  getGenres: "genre/movie/list",
+  nowPlaying: "movie/now_playing",
+  language: "language=en-US",
+  imageBase: "https://image.tmdb.org/t/p/w500",
+};
